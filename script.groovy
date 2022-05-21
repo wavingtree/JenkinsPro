b94fdf2d65ece942072c7626100b9505f7456d34
wavingtree <102631943+wavingtree@users.noreply.github.com>
2022-05-21 16:19:21 -0600

    create groovy script

---
+++ script.groovy
@@ -0,0 +1,16 @@
+def buildApp() {
+    echo 'building the application...'
+}
+
+def testApp() {
+    echo 'testing the application..'
+  
+}
+
+def deployApp() {
+    echo 'deploying the application...'
+    echo "deploying version ${params.VERSION}"
+}
+
+
+return this
